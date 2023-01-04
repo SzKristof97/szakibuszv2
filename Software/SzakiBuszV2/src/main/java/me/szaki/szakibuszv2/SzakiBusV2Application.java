c8a3951bfd234efda2c2ead8895546a5ca8c113b
Szabó Kristóf Gyula <szkristofgy@gmail.com>
2023-01-05 00:06:49 +0100

    Rename the project

--- Software/SzakiBuszV2/src/main/java/me/szaki/szakibuszv2/SzakiBusV2Application.java
+++ Software/SzakiBuszV2/src/main/java/me/szaki/szakibuszv2/SzakiBusV2Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SakiBusV2Application {
+public class SzakiBusV2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SakiBusV2Application.class, args);
+		SpringApplication.run(SzakiBusV2Application.class, args);
 	}
 
 }
