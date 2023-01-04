c8f6ec6a7e35e484929b5879957dd5e6920d1daf
Szabó Kristóf Gyula <szkristofgy@gmail.com>
2023-01-04 23:18:55 +0100

    First commit

---
+++ Software/SzakiBuszV2/src/main/java/me/szaki/szakibuszv2/SakiBusV2Application.java
@@ -0,0 +1,13 @@
+package me.szaki.szakibuszv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakiBusV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakiBusV2Application.class, args);
+	}
+
+}
---
+++ Software/SzakiBuszV2/src/test/java/me/szaki/szakibuszv2/SakiBusV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.szaki.szakibuszv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakiBusV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
